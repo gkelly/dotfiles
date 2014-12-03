7207fbb0aa4332143287642fdaaf5c87fa909b17
Garret Kelly <garret.kelly@gmail.com>
2014-12-03 19:40:26 +0000

    Add install.sh, to make installing things easy.

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+INSTALL='
+  .dircolors
+  .tmux.conf
+  .vim
+  .vimrc
+  .wmii
+  .zshrc
+'
+
+make_relative() {
+  local from=$1
+  local to=$2
+  python -c"
+import os.path
+print os.path.relpath('$from', '$to')
+"
+}
+
+where=$(make_relative $(dirname $0) $HOME)
+for i in $INSTALL; do
+  if [ -r $HOME/$i ]; then
+    echo "skip $i"
+    continue
+  fi
+  echo "link $i"
+  ln -s $where/$i $HOME/$i
+done
