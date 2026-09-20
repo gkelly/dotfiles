24189b43691a6fed7a7d58a37454251d214fe2b7
Garret Kelly <gkelly@gkel.ly>
2026-09-20 14:09:56 -0400

    zsh.nix: Add more eza aliases

--- zsh.nix
+++ zsh.nix
@@ -3,7 +3,13 @@
 {
   programs.zsh = {
     enable = true;
-    shellAliases.ls = "eza";
+    shellAliases = {
+      ls = "eza";
+      ll = "eza -l";
+      la = "eza -la";
+      l = "eza -lh --git";
+      tree = "eza --tree";
+    };
     initContent = ''
       autoload -Uz promptinit
       promptinit
